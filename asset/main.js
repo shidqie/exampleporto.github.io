b0a3acdf5fa5665219ad759771f1932ee904f9d5
Akhfa Shidqie Muttaqien <136985345+shidqie@users.noreply.github.com>
2023-08-09 23:43:23 +0700

    Add files via upload

---
+++ asset/main.js
@@ -0,0 +1,15 @@
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+});
